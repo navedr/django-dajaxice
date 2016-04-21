76d5c5f587027f45e8b81f737d0fd95e5405ef56
Naved Rangwala <rangwaln@advisory.com>
2016-04-20 23:04:29 -0400

    url fix

--- dajaxice/urls.py
+++ dajaxice/urls.py
@@ -5,7 +5,7 @@ except ImportError:
 
 from .views import DajaxiceRequest
 
-urlpatterns = ['dajaxice.views',
+urlpatterns = [
     url(r'^(.+)/$', DajaxiceRequest.as_view(), name='dajaxice-call-endpoint'),
     url(r'', DajaxiceRequest.as_view(), name='dajaxice-endpoint'),
 ]
